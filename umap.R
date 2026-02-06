b69ad092711c51a0b0bb6606bb2849c95ceb2425
rayoub <ronaldayoub@gmail.com>
2026-02-06 08:29:25 -0600

    commit

--- umap.R
+++ umap.R
@@ -12,15 +12,30 @@ source(here("R", "loading.R"))
 N_TOP_BETAS <- 32000 # top variably methylated probes
 
 # load betas and filter top 32,000 variably methylated probes
-betas <- loadSavedBetas(REF_GSE_ID)
-betas <- betas[, order(-apply(betas, 2, sd))[1:N_TOP_BETAS]]
+filtered_betas <- loadSavedBetas(REF_GSE_ID)
+filtered_betas <- filtered_betas[, order(-apply(filtered_betas, 2, sd))[1:N_TOP_BETAS]]
+
+saveRDS(filtered_betas, file=here("results", paste0(REF_GSE_ID, "_filtered_betas.rds")))
+
+anno <- loadSavedAnno(REF_GSE_ID)
+
+diag_id <- "DIAG1"
+betas <- loadSavedBetas(diag_id)
+
+sample_id <- "209794430029_R05C01"
+sample <- betas[sample_id,colnames(filtered_betas)]
 
 # load sample names and classes
-anno <- loadSavedAnno(REF_GSE_ID)
 labels <- data.frame(
-  Sample = anno$geo_accession,
-  Class = anno$`methylation class:ch1`
+  Sample = c(anno$geo_accession, sample_id),
+  Class = c(anno$`methylation class:ch1`, "CURRENT SAMPLE")
 )
+
+augmented_betas <- rbind(filtered_betas, sample)
+
+# args to function (sample, labels)
+
+
 
 # set seed
 seed <- 180314
@@ -28,17 +43,19 @@ set.seed(seed, kind = "L'Ecuyer-CMRG")
 
 # umap
 umap_res <- umap(
-  betas,
+  augmented_betas,
   n_neighbors = 30,
   min_dist = 0.75,
   metric = "euclidean"
 )
 
+# set names 
 umap_df <- as.data.frame(umap_res)
 colnames(umap_df) <- c("UMAP1", "UMAP2")
 rownames(umap_df) <- labels$Sample
 umap_df$Class <- labels$Class
 
+# calculate centroids for labels
 centroids <- umap_df |>
   group_by(Class) |>
   summarize(
@@ -46,9 +63,11 @@ centroids <- umap_df |>
     UMAP2 = mean(UMAP2)
   )
 
+
+
 # plot
 ggplot(umap_df, aes(x = UMAP1, y = UMAP2, color = Class)) +
-  geom_point(size = 0.9, alpha = 0.5) +
+  geom_point(size = 0.9, alpha = 0.5, show.legend = FALSE) +
   geom_text_repel(
     data = centroids,
     aes(label = Class),
@@ -57,7 +76,7 @@ ggplot(umap_df, aes(x = UMAP1, y = UMAP2, color = Class)) +
     color = "black",
     max.overlaps = Inf
   ) +
-  guides(color = guide_legend(ncol = 3, override.aes = list(size = 3))) +
+  #guides(color = guide_legend(ncol = 3, override.aes = list(size = 3))) +
   theme_minimal() +
   labs(title = "GSE90496 Reference Set") +
   theme(
